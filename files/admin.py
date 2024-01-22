10504d7a6f9d1a6db95567838c7e55c55fad1d90
u_alex90 <u_alex90@mail.ru>
2024-01-22 20:48:34 +1000

    Mod admin_models

--- files/admin.py
+++ files/admin.py
@@ -6,5 +6,7 @@ from files.models import File
 @admin.register(File)
 class FileAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'size', 'filename', 'is_public', 'status', 'owner', )
+    list_display_links = ('name', )
     search_fields = ('owner', 'name', )
     list_filter = ('is_public', 'status', )
+    list_editable = ('status', )
--- users/admin.py
+++ users/admin.py
@@ -5,7 +5,18 @@ from users.models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ('id', 'email', 'first_name', 'last_name', 'surname', 'date_joined', 'last_login', 'is_active', 'is_superuser')
-    # поиск
+    list_display = (
+        'id',
+        'email',
+        'first_name',
+        'last_name',
+        'surname',
+        'date_joined',
+        'last_login',
+        'is_active',
+        'is_staff',
+        'is_superuser')
+    list_display_links = ('email', 'first_name', 'last_name', 'surname', )
     search_fields = ('email', 'first_name', 'last_name', 'surname')
     list_filter = ('is_active', 'is_superuser', 'is_staff')
+    list_editable = ('is_active', 'is_superuser', 'is_staff')
