b0a563a6f4e782ed52082479a82e04f91b9264c0
u_alex90 <u_alex90@mail.ru>
2024-01-22 20:48:50 +1000

    Add ALLOWED_HOSTS

--- config/settings.py
+++ config/settings.py
@@ -33,7 +33,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '192.168.3.6',
+]
 
 # Application definition
 
