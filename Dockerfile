ccfcc79b6429498328c07f3fbf12ff104d763b8f
u_alex90 <u_alex90@mail.ru>
2024-01-31 21:00:10 +1000

    Refactor project

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM python:3.11-slim
 WORKDIR /code
 COPY pyproject.toml .
 COPY poetry.lock .
+COPY scripts/create_user.sh .
 RUN pip install --upgrade pip
 RUN pip install poetry
 RUN poetry config virtualenvs.create false
--- base/management/commands/ccsu.py
+++ base/management/commands/ccsu.py
@@ -1,19 +1,23 @@
 from django.core.management import BaseCommand
 
+from config import settings
 from users.models import User
 
 
 class Command(BaseCommand):
+    """
+    Cоздание пользователя для админ панели
+    """
 
     def handle(self, *args, **kwargs):
         user = User.objects.create(
-            email='admin@sky.pro',
+            email=settings.DJANGO_ADMIN_EMAIL,
             first_name='Admin',
-            last_name='SkyPro',
+            last_name='Django',
             is_staff=True,
             is_superuser=True,
             is_active=True,
         )
-
-        user.set_password('123qwe')
+        user.set_password(settings.DJANGO_ADMIN_PASSWORD)
         user.save()
+        print("Создан пользователь Django ADMIN")
