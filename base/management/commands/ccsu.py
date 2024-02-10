940ec2052e5b14f9f78d19efa989cb7bb86168f3
u_alex90 <u_alex90@mail.ru>
2024-02-10 13:58:09 +1000

    Fix ccsu

--- base/management/commands/ccsu.py
+++ base/management/commands/ccsu.py
@@ -8,16 +8,19 @@ class Command(BaseCommand):
     """
     Cоздание пользователя для админ панели
     """
-
-    def handle(self, *args, **kwargs):
-        user = User.objects.create(
-            email=settings.DJANGO_ADMIN_EMAIL,
-            first_name='Admin',
-            last_name='Django',
-            is_staff=True,
-            is_superuser=True,
-            is_active=True,
-        )
-        user.set_password(settings.DJANGO_ADMIN_PASSWORD)
-        user.save()
-        print("Создан пользователь Django ADMIN")
+    if not User.objects.get(email=settings.DJANGO_ADMIN_EMAIL):
+        def handle(self, *args, **kwargs):
+            user = User.objects.create(
+                email=settings.DJANGO_ADMIN_EMAIL,
+                first_name='Admin',
+                last_name='Django',
+                is_staff=True,
+                is_superuser=True,
+                is_active=True,
+            )
+            user.set_password(settings.DJANGO_ADMIN_PASSWORD)
+            user.save()
+            print("Создан пользователь Django ADMIN")
+    else:
+        def handle(self, *args, **kwargs):
+            print("Пользователь Django ADMIN уже существует")
